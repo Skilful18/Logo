8a4140035c6267dd46500fe2bb2d1d5e1a0d7210
Skilful18 <Skilful93@mail.ru>
2019-11-21 01:26:45 +0300

    Поправил мелочи + сверстал модальное окно

--- js/main.js
+++ js/main.js
@@ -17,4 +17,15 @@ $(document).ready(function() {
     $('.navbar__menu').toggleClass('navbar__menu-active'),
     $(".hero").toggleClass('hero-active')
   });
+  /* Модальные окна */
+  var button = $('#button');
+  var modal = $('#modal');
+  var close = $('#close');
+
+  button.on('click', function() {
+    modal.addClass('modal_active');
+  });
+  close.on('click', function() {
+    modal.removeClass('modal_active');
+  });
 });
